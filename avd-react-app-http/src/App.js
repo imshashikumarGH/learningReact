a328f6f10a1a513eefab63c513e2fe3b957e5f14
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-14 12:17:21 +0530

    Code refactor

--- avd-react-app-http/src/App.js
+++ avd-react-app-http/src/App.js
@@ -5,22 +5,25 @@ import { useState } from "react";
 function App() {
   const [movieList, setMovieList] = useState([]);
 
+
   async function fetchMovieHandler() {
     const response = await fetch("https://swapi.dev/api/films");
     const data = await response.json();
-    const movieList = data.results.map((movieData) => {
+    const movies = data.results.map((movieData) => {
       return {
         name: movieData.title,
         date: movieData.opening_crawl,
       };
     });
-    setMovieList(movieList);
+    setMovieList(movies);
   }
 
   return (
     <>
       <section>
         <button onClick={fetchMovieHandler}>Fetch deatils</button>
+        </section>
+        <section>
         <Movie movieList={movieList} />
       </section>
     </>
