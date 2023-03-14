0f5ed6b582831393e132ef6664e8489e30f21242
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-14 12:07:19 +0530

    Feature HTTP GET request fetch details

--- avd-react-app-http/src/App.js
+++ avd-react-app-http/src/App.js
@@ -1,11 +1,34 @@
+import Movie from "./component/Movie";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+
+  const fetchMovieHandler = () => {
+    fetch("https://swapi.dev/api/films").then((response) => {
+      return response.json();
+    })
+    .then(data => {
+      const movieList = data.results.map(movieData => {
+        return {
+          name : movieData.title,
+          date:movieData.opening_crawl
+        };
+      });
+      setMovieList(movieList)
+    })
+  };
+
   return (
-    <div >
-      <header >
-          Learn React
-      </header>
-    </div>
+    <>
+    
+      <section>
+      <button onClick={fetchMovieHandler}>Fetch deatils</button>
+        <Movie movieList={movieList} />
+      </section>
+      
+    </>
   );
 }
 
