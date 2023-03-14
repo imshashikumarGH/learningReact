9c1933231979c89c9eb690b068f50433bb5a809c
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-14 13:14:03 +0530

    Feature HTTP :Error handling in APi with try and catch

--- avd-react-app-http/src/App.js
+++ avd-react-app-http/src/App.js
@@ -4,30 +4,39 @@ import { useState } from "react";
 
 function App() {
   const [movieList, setMovieList] = useState([]);
-  const [isLoading,setIsloading] = useState(false)
+  const [isLoading, setIsloading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMovieHandler() {
-    setIsloading(true)
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
-    const movies = data.results.map((movieData) => {
-      return {
-        name: movieData.title,
-        date: movieData.opening_crawl,
-      };
-    });
-    setMovieList(movies);
-    setIsloading(false)
+    setIsloading(true);
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
+      if (!response.ok) {
+        throw new Error("Something went wrong while hitting API");
+      }
+      const data = await response.json();
+      const movies = data.results.map((movieData) => {
+        return {
+          name: movieData.title,
+          date: movieData.opening_crawl,
+        };
+      });
+      setMovieList(movies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsloading(false);
   }
 
   return (
     <>
       <section>
         <button onClick={fetchMovieHandler}>Fetch deatils</button>
-        </section>
-        <section>
+      </section>
+      <section>
         {!isLoading && movieList.length >= 0 && <Movie movieList={movieList} />}
-        {!isLoading &&  movieList.length === 0 && <p>No movie deatils... </p>}
+        {!isLoading && movieList.length === 0 && !error && <p>No movie deatils... </p>}
+        {!isLoading && error && <p>{error}</p>}
         {isLoading && <p>Loading... </p>}
       </section>
     </>
