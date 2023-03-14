39ea564b811968c98f9c0fc0b6305afa58f0694c
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-14 12:13:22 +0530

    Feature HTTP :  Sync Await Js Codes

--- avd-react-app-http/src/App.js
+++ avd-react-app-http/src/App.js
@@ -5,29 +5,24 @@ import { useState } from "react";
 function App() {
   const [movieList, setMovieList] = useState([]);
 
-  const fetchMovieHandler = () => {
-    fetch("https://swapi.dev/api/films").then((response) => {
-      return response.json();
-    })
-    .then(data => {
-      const movieList = data.results.map(movieData => {
-        return {
-          name : movieData.title,
-          date:movieData.opening_crawl
-        };
-      });
-      setMovieList(movieList)
-    })
-  };
+  async function fetchMovieHandler() {
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+    const movieList = data.results.map((movieData) => {
+      return {
+        name: movieData.title,
+        date: movieData.opening_crawl,
+      };
+    });
+    setMovieList(movieList);
+  }
 
   return (
     <>
-    
       <section>
-      <button onClick={fetchMovieHandler}>Fetch deatils</button>
+        <button onClick={fetchMovieHandler}>Fetch deatils</button>
         <Movie movieList={movieList} />
       </section>
-      
     </>
   );
 }
